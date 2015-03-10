1c7a40dbc02c6e47e143e55ba845f64246a3c2e6
Dennis Lu <dennislu@fashionguide.com.tw>
2015-03-10 19:03:35 +0800

    welcome

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,26 @@
 class ChannelsController < ApplicationController
+
+	before_action :set_channel, only: [:show, :edit, :update, :destroy]
+
+	def index
+		
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
